9b9603ba3ea15976018a420412357a5a142c8706
romuloabbiati <rhabbiati@hotmail.com>
2023-09-18 06:53:29 +0100

    final

--- src/pages/CepSearch/index.tsx
+++ src/pages/CepSearch/index.tsx
@@ -1,16 +1,44 @@
 import './styles.css';
 
 import ResultCard from 'components/ResultCard';
+import { useState } from 'react';
+import axios from 'axios';
+
+type FormData = {
+  cep: string;
+};
+
+type Address = {
+  logradouro: string;
+  localidade: string;
+};
 
 const CepSearch = () => {
+  const [address, setAddress] = useState<Address>();
+
+  const [formData, setFormData] = useState<FormData>({
+    cep: '',
+  });
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log("Updated: " + event.target.value);
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setFormData({ ...formData, [name]: value });
   };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log("Submitted!");
+    
+    axios.get(`https://viacep.com.br/ws/${formData.cep}/json/`)
+      .then((response) => {
+        setAddress(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        setAddress(undefined);
+        console.log(error);
+      });
   };
 
   return (
@@ -21,6 +49,8 @@ const CepSearch = () => {
           <div className="form-container">
             <input
               type="text"
+              name="cep"
+              value={formData.cep}
               className="search-input"
               placeholder="CEP (somente números)"
               onChange={handleChange}
@@ -30,10 +60,12 @@ const CepSearch = () => {
             </button>
           </div>
         </form>
-
-        <ResultCard title="Logradouro" description="Lalala" />
-        <ResultCard title="Número" description="234" />
-
+        {address && (
+          <>
+            <ResultCard title="Logradouro" description={address.logradouro} />
+            <ResultCard title="Localidade" description={address.localidade} />
+          </>
+        )}
       </div>
     </div>
   );
